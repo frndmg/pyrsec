28b7eab2bfcdb8b45ff1a615f7c41f0748666eb4
Fernando Martínez González <frndmartinezglez@gmail.com>
2023-03-11 17:28:03 +0100

    Remove commented line

--- tests/test_json.py
+++ tests/test_json.py
@@ -16,7 +16,6 @@ def parser() -> Parsec[JSON]:
     number = Parsec.from_re(re.compile(r"-?\d+")).map(int)
     null = Parsec.from_re(re.compile(r"null")).map(lambda _: None)
     quote = Parsec.from_re(re.compile('"')).map(lambda _: None)
-    # quote = Parsec.from_func(lambda s: (None, s[1:]) if s[:1] == '"' else None)
     string = quote >> Parsec.from_re(re.compile(r"[^\"]*")) << quote
     return true | false | number | null | string
 
