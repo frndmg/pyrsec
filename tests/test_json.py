a97f0387365a531ccaea9a491e41036ac70627f6
Fernando Martínez González <frndmartinezglez@gmail.com>
2023-03-11 18:08:38 +0100

    Add list to the json parser implementation

--- tests/test_json.py
+++ tests/test_json.py
@@ -29,13 +29,28 @@ json_strategy = strategies.from_type(JSON)
 
 @pytest.fixture(scope="session")
 def parser() -> Parsec[JSON]:
+    json_parser: Parsec[JSON]
+
     true = Parsec.from_re(re.compile(r"true")).map(lambda _: True)
     false = Parsec.from_re(re.compile(r"false")).map(lambda _: False)
     number = Parsec.from_re(re.compile(r"-?\d+")).map(int)
     null = Parsec.from_re(re.compile(r"null")).map(lambda _: None)
+
     quote = Parsec.from_re(re.compile('"')).map(lambda _: None)
     string = quote >> Parsec.from_re(re.compile(r"[^\"]*")) << quote
-    return true | false | number | null | string
+
+    opened_square_bracket = Parsec.from_string("[")
+    closed_square_bracket = Parsec.from_string("]")
+
+    list_ = (
+        opened_square_bracket
+        >> Parsec.from_deferred(lambda: json_parser)
+        << closed_square_bracket
+    )
+
+    json_parser = true | false | number | null | string | list_
+
+    return json_parser
 
 
 def test_json_single(parser: Parsec[JSON]) -> None:
