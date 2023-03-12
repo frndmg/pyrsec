c6c30584581323579bf144c7d35a938a04d53e1c
Fernando Martínez González <frndmartinezglez@gmail.com>
2023-03-12 04:15:02 +0100

    Bump version to 0.2.0

--- pyrsec/__init__.py
+++ pyrsec/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import re
 from typing import Any, Callable, Generic, List, Protocol, Tuple, TypeVar
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 T = TypeVar("T", covariant=True)
 
